84be83a0a8a1141406726735cd19d697b75bef40
Leo Zarni <leokhantzarni@gmail.com>
2025-03-02 23:22:32 +1100

    added git-conflict

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,1 +1,1 @@
-return { "akinsho/git-conflict.nvim", version = "*", config = true }
+return { "akinsho/git-conflict.nvim", version = "v2.1.0", config = true }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
       "akinsho/toggleterm.nvim",
       version = "*",
       opts = {
-        direction = "horizontal",
+        direction = "float",
         open_mapping = [[<c-\>]],
         hide_numbers = true,
         insert_mappings = true,
