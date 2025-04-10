690965d673afde9630a6dedfba70490b55ea1855
Leo Zarni <leokhantzarni@gmail.com>
2025-04-11 00:02:58 +1000

    AASGFASFASFDS

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,43 @@
+-- return {
+--   -- add gruvbox
+--   -- { "ellisonleao/gruvbox.nvim" },
+--   -- { "AlphaTechnolog/onedarker.nvim" },
+--   -- { "lunarvim/Onedarker.nvim" },
+--   -- { "cocopon/iceberg.vim" },
+--   -- { "EdenEast/nightfox.nvim" }, -- lazy
+--   { "catppuccin/nvim", name = "catppuccin" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
 return {
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- { "AlphaTechnolog/onedarker.nvim" },
-  -- { "lunarvim/Onedarker.nvim" },
-  -- { "cocopon/iceberg.vim" },
-  { "EdenEast/nightfox.nvim" }, -- lazy
-  -- { "catppuccin/nvim", name = "catppuccin" },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "nordfox",
-    },
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
   },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "frappe", -- Set flavor here
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
 }
