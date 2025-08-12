9ea58bf0db0982d117a2dac49d27be3ae3860935
Leo Zarni <leokhantzarni@gmail.com>
2025-08-12 22:39:56 +1000

    ASCHKLABKA

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,63 +1,13 @@
--- return {
---   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
---   {
---     "baliestri/aura-theme",
---     lazy = false,
---     priority = 1000,
---     config = function(plugin)
---       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
---       vim.cmd([[colorscheme aura-dark]])
---     end,
---   },
--- }
+local selected = "gruvbox-material"
 
---   -- add gruvbox
---   -- { "ellisonleao/gruvbox.nvim" },
---   -- { "AlphaTechnolog/onedarker.nvim" },
---   -- { "lunarvim/Onedarker.nvim" },
---   -- { "cocopon/iceberg.vim" },
---   -- { "EdenEast/nightfox.nvim" }, -- lazy
---   { "catppuccin/nvim", name = "catppuccin" },
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "catppuccin",
---     },
---   },
--- }
--- return {
---   {
---     "sainnhe/gruvbox-material",
---     lazy = false,
---     priority = 1000,
---     config = function()
---       -- Optionally configure and load the colorscheme
---       -- directly inside the plugin declaration.
---       vim.g.gruvbox_material_enable_italic = true
---       vim.cmd.colorscheme("gruvbox-material")
---     end,
---   },
--- }
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- Set flavor here
-        color_overrides = {
-          mocha = {
-            base = "#000000",
-            mantle = "#000000"
-          },
-        }
-      })
-    end,
-  },
+  require("plugins.colorschemes.aura-theme"),
+  require("plugins.colorschemes.gruvbox-material"),
+  require("plugins.colorschemes.vercel"),
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-    },
+      colorscheme = selected
+    }
   }
 }
--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -2,6 +2,13 @@ return {
   "OXY2DEV/markview.nvim",
   lazy = false, -- Recommended
   -- ft = "markdown" -- If you decide to lazy-load anyway
+  config = function()
+    require("markview").setup({
+      experimental = {
+        check_rtp_message = false,
+      }
+    })
+  end,
 
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
