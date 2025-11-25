e3445c23b0f16649a102794e06b02bb01632a244
Khant Zarni <khantzarni@Leos-Laptop.local>
2025-11-26 00:18:23 +1100

    added tokyonight to colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,6 @@
-local selected = "gruvbox-material"
-
 return {
+  require("plugins.colorschemes.vercel"),
   require("plugins.colorschemes.aura-theme"),
   require("plugins.colorschemes.gruvbox-material"),
-  require("plugins.colorschemes.vercel"),
-  require("plugins.colorschemes.vesper"),
-  {
-    "LazyVim/LazyVim",
-    opts = {
-            colorscheme = selected
-    }
-  }
+  require("plugins.colorschemes.tokyonight"),
 }
